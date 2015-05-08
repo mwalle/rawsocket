3d61c26e041374d3145612da3c3f1b7d338d6d66
Michael Walle <michael@walle.cc>
2015-05-08 16:59:00 +0200

    initial commit
    Signed-off-by: Michael Walle <michael@walle.cc>

---
+++ rawsocket.c
@@ -0,0 +1,134 @@
+/*
+ * Python module to create raw sockets as unprivileged user.
+ *
+ * Copyright (c) 2015, Michael Walle <michael@walle.cc>
+ * See LICENSE for licensing terms.
+ */
+
+#include <Python.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+
+#define RAW_SOCK_HELPER "rawsocket_helper"
+
+static int spawn_helper(int fd)
+{
+	int status;
+	pid_t pid;
+
+	pid = fork();
+	if (pid < 0) {
+		PyErr_SetFromErrno(PyExc_IOError);
+		return -1;
+	}
+
+	if (pid == 0) {
+		/* child */
+		char fd_str[8];
+		char *argv[] = { RAW_SOCK_HELPER, fd_str, NULL };
+		char *envp[] = { NULL };
+
+		snprintf(fd_str, sizeof(fd_str), "%d", fd);
+		execve(argv[0], argv, envp);
+		exit(1);
+	}
+
+	waitpid(pid, &status, 0);
+	if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+		return 0;
+
+	PyErr_Format(PyExc_IOError, "Could not spawn helper '%s'.", RAW_SOCK_HELPER);
+	return -1;
+}
+
+static int raw_socket(void)
+{
+	int sv[2];
+	int rc;
+	struct iovec iov;
+	struct msghdr msg = {0};
+	struct cmsghdr *cmsg;
+	char buf[CMSG_SPACE(sizeof(int))];
+	char dummy;
+
+	/* dummy read */
+	iov.iov_base = &dummy;
+	iov.iov_len = 1;
+
+	msg.msg_name = NULL;
+	msg.msg_namelen = 0;
+	msg.msg_control = buf;
+	msg.msg_controllen = sizeof(buf);
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+
+	/* create communication socket */
+	rc = socketpair(AF_UNIX, SOCK_DGRAM, 0, sv);
+	if (rc == -1) {
+		PyErr_SetFromErrno(PyExc_IOError);
+		return -1;
+	}
+
+	/* fork our privileged helper program */
+	rc = spawn_helper(sv[1]);
+	if (rc == -1)
+		return -1;
+
+	/* receive the privileged socket */
+	rc = recvmsg(sv[0], &msg, 0);
+	if (rc == -1) {
+		PyErr_SetFromErrno(PyExc_IOError);
+		return -1;
+	}
+
+	cmsg = CMSG_FIRSTHDR(&msg);
+	if (!cmsg
+			|| cmsg->cmsg_len != CMSG_LEN(sizeof(int))
+			|| cmsg->cmsg_level != SOL_SOCKET
+			|| cmsg->cmsg_type != SCM_RIGHTS) {
+		PyErr_SetFromErrno(PyExc_IOError);
+		return -1;
+	}
+
+	close(sv[0]);
+	close(sv[1]);
+
+	return *((int *)CMSG_DATA(cmsg));
+}
+
+static PyObject *
+rawsocket_fd(PyObject *self, PyObject *args)
+{
+	int fd;
+
+	if (!PyArg_ParseTuple(args, ""))
+		return NULL;
+
+	fd = raw_socket();
+	if (fd < 0)
+		return NULL;
+
+	return PyInt_FromLong(fd);
+}
+
+PyDoc_STRVAR(rawsocket_fd_doc,
+"rawsocket_fd()\n\
+\n\
+Create a new raw socket using a privileged helper program.");
+
+static PyMethodDef rawsocket_methods[] = {
+	{"rawsocket_fd", rawsocket_fd,
+	 METH_VARARGS, rawsocket_fd_doc},
+	{NULL, NULL}
+};
+
+PyMODINIT_FUNC
+initrawsocket(void)
+{
+	PyObject *m;
+
+	m = Py_InitModule("rawsocket", rawsocket_methods);
+	if (m == NULL)
+		return;
+}
---
+++ rawsocket_helper.c
@@ -0,0 +1,88 @@
+/*
+ * Privileged helper binary.
+ *
+ * Copyright (c) 2015, Michael Walle <michael@walle.cc>
+ * See LICENSE for licensing terms.
+ *
+ * Quick guide:
+ *   make rawsocket_helper
+ *   chown root:yourgroup rawsocket_helper
+ *   chmod 750 rawsocket_helper
+ *   setcap cap_net_raw+ep rawsocket_helper
+ */
+
+#include <arpa/inet.h>
+#include <net/ethernet.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int send_socket(int fd, int sfd)
+{
+	int rc;
+	struct iovec iov;
+	struct msghdr msg = {0};
+	struct cmsghdr *cmsg;
+	char buf[CMSG_SPACE(sizeof(int))];
+
+	/* dummy write */
+	iov.iov_base = "";
+	iov.iov_len = 1;
+
+	msg.msg_control = buf;
+	msg.msg_controllen = sizeof(buf);
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+	cmsg = CMSG_FIRSTHDR(&msg);
+	cmsg->cmsg_level = SOL_SOCKET;
+	cmsg->cmsg_type = SCM_RIGHTS;
+	cmsg->cmsg_len = CMSG_LEN(sizeof(int));
+	*(int *)CMSG_DATA(cmsg) = sfd;
+
+	rc = sendmsg(fd, &msg, 0);
+	if (rc == -1)
+		return -1;
+
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	int rc;
+	int fd, raw_fd;
+	char *endp;
+	int protocol;
+	int ret = EXIT_FAILURE;
+
+	/* argument parsing */
+	if (argc < 2 || argc > 3)
+		goto out;
+
+	if (argc == 3) {
+		protocol = strtol(argv[2], &endp, 10);
+		if (*argv[2] && *endp)
+			goto out;
+	} else {
+		protocol = ETH_P_ALL;
+	}
+
+	fd = strtol(argv[1], &endp, 10);
+	if (*endp)
+		goto out;
+
+	/* open raw socket */
+	raw_fd = socket(AF_PACKET, SOCK_RAW, htons(protocol));
+	if (raw_fd == -1)
+		goto out;
+
+	rc = send_socket(fd, raw_fd);
+	if (rc == -1) {
+		close(raw_fd);
+		goto out;
+	}
+
+	ret = EXIT_SUCCESS;
+
+out:
+	close(fd);
+	return ret;
+}
